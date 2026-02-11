9b2dfd411d35dbb6b801551279a467e22300dfde
qevolg <2227465945@qq.com>
2026-02-11 16:40:40 +0800

    feat: refactor test-sbom

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use taos::{AsyncTBuilder, TaosBuilder};
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let _taos = TaosBuilder::from_dsn("ws://localhost:6041")?
-        .build()
-        .await?;
-
+fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
     Ok(())
 }
